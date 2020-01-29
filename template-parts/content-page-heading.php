9ff5d822673aebfff08fdd162f08899cf51ecef8
Matt Hummel <marketing@users.noreply.github.com>
2020-01-29 17:07:47 -0500

    Update
    Page heading typography updates

--- template-parts/content-page-heading.php
+++ template-parts/content-page-heading.php
@@ -8,7 +8,6 @@
   
         <div class="crumbs-container">
 
-  
             <nav aria-label="<?php _e('You are here:', 'gcc-wp-2018');?>">
               <?php custom_breadcrumbs();?>
             </nav>
