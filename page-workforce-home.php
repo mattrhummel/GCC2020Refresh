b66ab99146a454230a7bfb2e682bb6738ffecfa5
Matt Hummel <marketing@users.noreply.github.com>
2020-02-03 14:21:16 -0500

    Update
    Remove workforce newsletter signup from home page.

--- page-workforce-home.php
+++ page-workforce-home.php
@@ -147,24 +147,6 @@ get_header(); ?>
             <p class="description"><?php the_field('box_5_description');  ?></p>
             <a href="<?php the_field('box_5_url');?>" class="button primary" ><?php  the_field('box_5_button_text'); ?></a>
           </div>
-
-        
-        <div class="simple-subscription-form">
-          <form action="<?php the_field('sign_up_box_action_url', 113);?>" method="post" >
-            <div class="row expanded">
-              
-              <h2><?php the_field('sign_up_box_heading', 113);?></h2>
-              <p><?php the_field('sign_up_box_description', 113); ?></p>
-              <div class="input-group">
-                <span class="input-group-label">
-                  <span class="fa fa-envelope"></span>
-                </span>
-                <input class="input-group-field" type="email" placeholder="<?php esc_html_e('Email', 'gcc-wp-2018'); ?>" required>
-                <input type="submit" name="go" value="<?php the_field('sign_up_box_button_text', 113)?>" title="<?php the_field('sign_up_box_button_text',  113)?>" class="button secondary"/>
-              </div>
-            </div>
-          </form>
-        </div>
         
   <div data-equalizer data-equalize-by-row="true" data-equalize-on="medium">
           
