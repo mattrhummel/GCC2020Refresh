e666fd9ab28e7c4bf15f65dd5e591dd79c91e10e
Matt Hummel <marketing@users.noreply.github.com>
2020-02-04 15:29:20 -0500

    update

--- archive.php
+++ archive.php
@@ -17,7 +17,7 @@ get_header();?>
 
       <div class="row header-content">
      
-        <?php the_title(); ?>
+        <h1><?php echo get_cat_name(1); ?></h1>
   
         <div class="crumbs-container">
 
