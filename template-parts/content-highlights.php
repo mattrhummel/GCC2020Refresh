30373a7d3264424f5b74bb4a4963eee02385e7b5
Matt Hummel <marketing@users.noreply.github.com>
2020-02-06 08:47:28 -0500

    Update
    Fix the post date

--- template-parts/content-highlights.php
+++ template-parts/content-highlights.php
@@ -34,7 +34,7 @@ $highlights_heading = get_field('highlights_heading');
 
             <h3 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
             
-          <p><?php the_date(); ?></p>
+            <p><?php echo get_the_date();  ?></p>
 
           </div>
 
