62104b2b0cd42d42df71fbd6da5d635dc569f259
Matt Hummel <marketing@users.noreply.github.com>
2020-02-10 16:22:05 -0500

    Update
    Remove blog breadcrumbs

--- archive.php
+++ archive.php
@@ -21,9 +21,6 @@ get_header();?>
   
         <div class="crumbs-container">
 
-            <nav aria-label="<?php _e('You are here:', 'gcc-wp-2018');?>">
-              <?php custom_breadcrumbs();?>
-            </nav>
     
         </div>
  
--- index.php
+++ index.php
@@ -23,10 +23,7 @@ get_header();?>
         <h1 class="entry-title"><?php echo get_cat_name(1); ?></h1>
         <div class="crumbs-container">
 
-            <nav aria-label="<?php _e('You are here:', 'gcc-wp-2018');?>">
-              <?php custom_breadcrumbs();?>
-            </nav>
-    
+        
         </div>
 
     </div>
