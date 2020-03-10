af61217b725a04bc30c6dcf385267248bb188a9a
Matt Hummel <marketing@users.noreply.github.com>
2020-03-10 15:57:06 -0400

    Archive
    Fix archive not found

--- archive.php
+++ archive.php
@@ -55,6 +55,7 @@ if ( have_posts() ) : ?>
           <a href="<?php the_permalink(); ?>">
             <?php the_title(); ?>
           </a>
+          <?php the_date(); ?>
           </h3>
           <?php if ( 'post' === get_post_type() ) : ?>
           
@@ -79,8 +80,55 @@ if ( have_posts() ) : ?>
       <?php endwhile;
       the_posts_navigation(); ?>
     </div>
-    <?php else :
-    get_template_part( 'template-parts/content', 'none' ); ?>
+    <?php else : ?>
+
+
+ <div class="row content-area">
+
+ <div class="columns small-12 medium-12 large-8 float-right">
+      
+        <div class="entry-content" id="main">
+
+    <h1 class="page-title"><?php esc_html_e( 'Nothing found for this event category', 'gcc-wp-2018' ); ?></h1>
+
+    <?php
+    if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+
+      <p><?php
+        printf(
+          wp_kses(
+            /* translators: 1: link to WP admin new post page. */
+            __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'gcc-wp-2018' ),
+            array(
+              'a' => array(
+                'href' => array(),
+              ),
+            )
+          ),
+          esc_url( admin_url( 'post-new.php' ) )
+        );
+      ?></p>
+
+    <?php elseif ( is_search() ) : ?>
+
+      <p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'gcc-wp-2018' ); ?></p>
+      <?php
+        get_search_form();
+
+    else : ?>
+
+      <p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'gcc-wp-2018' ); ?></p>
+      <?php
+
+
+    endif; ?>
+
+  </div>
+  </div>
+
+</div>
+
+
     <?php endif; ?>
   </div>
 </div>
